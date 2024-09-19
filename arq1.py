e9e2b444806be86726e66a68dd096d68f85b06be
Asafe0007 <0000110410071xsp@al.educacao.sp.gov.br>
2024-09-19 16:19:10 -0300

    Oi

---
+++ arq1.py
@@ -0,0 +1,10 @@
+git config --global user.name Asafe
+git config --global user.email asafexsp@al.educacao.sp.gov.br
+
+git config --global user.email "asafexsp@al.educacao.sp.gov.br"
+
+git clone link_do_repositorio
+git add . ou git add nome_do_arquivo
+git commit -m 	"mensagem"
+git status
+git push	
